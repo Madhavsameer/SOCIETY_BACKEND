1c50a053646a46cda633807ca2be052c5462caa9
Madhav sameer <111692125+Madhavsameer@users.noreply.github.com>
2024-11-19 07:30:39 +0530

    Delete src directory

--- src/main/java/com/inf/repository/AdminProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inf.repository;
-
-import com.inf.model.AdminProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminProfileRepository extends JpaRepository<AdminProfile, Long> {
-}
--- src/main/java/com/inf/repository/ResidentProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inf.repository;
-
-import com.inf.model.ResidentProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResidentProfileRepository extends JpaRepository<ResidentProfile, Long> {
-}
